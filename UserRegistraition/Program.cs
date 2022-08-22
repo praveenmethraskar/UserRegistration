034165ca57d0cfaabcc129eb234045b27c75d6a6
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-22 20:33:51 +0530

    Validating name

--- UserRegistraition/Program.cs
+++ UserRegistraition/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using UserRegistraition;
+
+namespace UserRegistration
+{
+
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            User user = new User();
+            user.ValidFirstName();
+        }
+    }
+}
---
+++ UserRegistraition/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistraition
+{
+    public class User
+    {
+        public void ValidFirstName()
+        {
+            Console.WriteLine("enter First name");
+            string data = Console.ReadLine();
+            string namepattern = "^[A-Z]{1}[a-zA-Z]{2,}";
+
+            Regex regex = new Regex(namepattern);
+
+            if (regex.IsMatch(data))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Enter name is valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Entered name is not valid");
+                Console.ResetColor();
+            }
+            Console.WriteLine(data);
+
+
+        }
+    }
+}
